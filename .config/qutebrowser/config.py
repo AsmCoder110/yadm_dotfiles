45d5d76a2db0a3cd932a1326a5a5651e2f420bf3
AsmCoder110 <yskthecpplover@gmail.com>
2019-10-21 01:17:36 +0530

    Remove some, add some

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,35 +1,45 @@
 config.load_autoconfig()
 c.auto_save.session = True
-config.bind(',cp', 'open -t https://cpplang.slack.com')
-config.bind(',wb', 'open -t https://wandbox.org')
 config.bind(',js', 'open -t https://jisho.org')
-config.bind(',fb', 'open -t https://facebook.com')
-config.bind(',ff', 'open -t https://10fastfingers.com/top1000')
 config.bind(';cr', 'open -t https://en.cppreference.com')
 config.bind(',wp', 'open -t https://www.reddit.com/r/wallpaper')
 config.bind(',pc', 'open -t https://www.reddit.com/r/programmingcirclejerk')
 config.bind(',gh', 'open -t https://github.com')
 config.bind(',tr', 'open -t https://nitrotype.com/race')
 config.bind(',kh', 'open -t https://www.keyhero.com/free-typing-test')
-config.bind(',rd', 'open -t https://www.reddit.com')
-config.bind(',tw', 'open -t https://www.twitter.com')
 config.bind(',cm', 'open -t https://cmake.org/cmake/help/latest/')
 config.bind(',ra', 'spawn --userscript readability')
 config.bind(',v' , 'spawn --userscript view_in_mpv')
 config.bind('e', 'hint links spawn --userscript /home/asmcoder/.config/qutebrowser/userscripts/view_in_mpv {hint-url}')
 
+c.fonts.web.family.fantasy = 'SourceHanSansJP-Medium'
+c.fonts.web.family.standard = 'SourceHanSansJP-Medium'
+c.fonts.web.family.cursive = 'SourceHanSansJP-Medium'
+c.fonts.web.family.sans_serif = 'SourceHanSansJP-Medium'
+c.fonts.web.family.serif = 'SourceHanSansJP-Medium'
+c.fonts.web.family.fixed = 'SourceHanSansJP-Medium'
+c.fonts.hints = '12pt SourceHanSansJP-Bold'
+c.fonts.prompts = 'SourceHanSansJP-Medium'
+
+
+c.content.proxy='socks://localhost:9050'
 c.scrolling.smooth = True
 c.content.headers.do_not_track = True
 c.downloads.remove_finished = 0
+c.downloads.location.prompt = False
+c.downloads.location.remember = False
+c.content.webrtc_ip_handling_policy = 'disable-non-proxied-udp'
+c.content.headers.user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
 config.unbind("gd", mode='normal')
 config.bind(
     'gd', 'set downloads.location.directory ~/Downloads ;; hint links download')
-config.bind(
-    'gp', 'set downloads.location.directory ~/Pictures/Wallpapers ;; hint links download')
 
 c.url.searchengines['gh']  = 'https://github.com/search?q={}'
+c.url.searchengines['ad']  = 'https://developer.android.com/s/results/?q={}'
 c.url.searchengines['gl']  = 'https://gitlab.com/search?search={}'
 c.url.searchengines['od']  = 'https://www.google.com/search?q={}+intitle:%22index%20of%22%20-inurl:(jsp%7Cpl%7Cphp%7Chtml%7Caspx%7Chtm%7Ccf%7Cshtml)%20-inurl:(listen77%7Cmp3raid%7Cmp3toss%7Cmp3drug%7Cindex_of%7Cwallywashis)'
+
+c.url.searchengines['cpp']  = 'en.cppreference.com/mwiki/index.php?search={}'
 
 c.url.searchengines['r']   = 'https://www.reddit.com/search?q={}'
 c.url.searchengines['r/']  = 'https://www.reddit.com/r/{}'
@@ -40,6 +50,7 @@ c.url.searchengines['j']   = 'https://www.jisho.org/search/{}'
 
 c.url.searchengines['y']   = 'https://www.youtube.com/results?search_query={}'
 c.url.searchengines['gt']   = 'https://translate.google.com/#view=home&op=translate&sl=auto&tl=en&text={}'
+c.url.searchengines['ka'] = 'https://app.kanjialive.com/{}'
 
 c.url.searchengines['w']   = 'https://en.wikipedia.org/w/index.php?search={}'
 c.url.searchengines['wit'] = 'https://it.wikipedia.org/w/index.php?search={}'
@@ -62,4 +73,28 @@ c.aliases['q'] = 'quit --save'
 
 c.content.mute = True
 c.content.host_blocking.lists = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts", "https://raw.githubusercontent.com/anudeepND/youtubeadsblacklist/master/hosts.txt", "https://raw.githubusercontent.com/kbinani/adblock-youtube-ads/master/signed.txt"]
-config.source('qutewal.py')
+c.content.javascript.enabled = False
+c.content.cookies.accept='no-3rdparty'
+c.content.cookies.store=True
+config.set('content.javascript.enabled', True, 'https://github.com')
+config.set('content.javascript.enabled', True, 'https://gitlab.com')
+config.set('content.javascript.enabled', True, 'https://duckduckgo.com')
+config.set('content.javascript.enabled', True, 'https://www.youtube.com')
+config.set('content.javascript.enabled', True, 'https://developer.android.com')
+config.set('content.javascript.enabled', True, 'https://*.stackexchange.com')
+config.set('content.javascript.enabled', True, 'https://jisho.org')
+config.set('content.javascript.enabled', True, 'https://*.keyhero.com')
+config.set('content.javascript.enabled', True, 'https://www.reddit.com')
+config.set('content.javascript.enabled', True, 'https://translate.google.com')
+config.set('content.javascript.enabled', True, 'https://gcc.godbolt.org')
+
+# Keep the hints consistent.
+c.colors.hints.bg = 'black'
+c.colors.hints.fg = 'white'
+
+#config.set('content.user_stylesheets', ['./custom_solarized.css', './solarized-dark.css'], '*://github.com')
+#config.set('content.user_stylesheets', ['./custom_solarized.css', './solarized-dark.css'], '*://stackoverflow.com')
+#config.set('content.user_stylesheets', ['./custom_solarized.css', './solarized-dark.css'], '*://en.wiktionary.org')
+#config.set('content.user_stylesheets', ['./custom_solarized.css', './solarized-dark.css'], '*://wiki.archliinux.org')
+#config.set('content.user_stylesheets', ['./custom_solarized.css', './solarized-dark.css'], '*://en.cppreference.com')
+#config.set('content.user_stylesheets', ['./custom_solarized.css', './solarized-dark.css'], '*://*.duckduckgo.com')
